f02f280670ab7ee340cf9063ad4543c4a59f4170
dpsumner1289 <dpsumner1289@gmail.com>
2016-10-17 12:17:54 -0400

    Create center-vertically.js

---
+++ center-vertically.js
@@ -0,0 +1,29 @@
+// Take an element (<div>, etc) and vertically center it within its container, dynamically and when resizing window.
+// Container must be positioned relatively and inner element must be positioned absolutely, or at least this is how I utilized it.
+
+jQuery(document).ready(function($) {
+	
+		function setMargin(el, eltwo){
+			var bannerHeight = $(eltwo).map(function() {
+			  return $(this).height();
+			}).get();
+			var height = $(el).map(function() {
+			  return $(this).height();
+			}).get();
+
+			var sectHeight = Math.max.apply(null, height);
+			var contHeight = Math.max.apply(null, bannerHeight);
+			var heightDiff = (contHeight - sectHeight) / 2;
+			$(el).css('margin-top', heightDiff);
+		}
+
+		var containerDiv;
+		var innerDiv;
+
+		setMargin(containerDiv, innerDiv);
+
+	$(window).on('resize', function(){
+		setMargin(containerDiv, innerDiv);
+	});
+  
+});
